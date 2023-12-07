45d71a6d508c46c4d6a08e126ba0974219c73f00
LiQiongchao <liqiongchao2012@163.com>
2023-12-07 20:27:30 +0800

    fear: 增加 model

---
+++ sub-project03/src/main/java/com/yupop/root/Main.java
@@ -0,0 +1,12 @@
+package com.yupop.root;
+
+/**
+ * @Author: Liqc
+ * @Date: 2023/12/7 20:08
+ * @Version: 1.0
+ */
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
